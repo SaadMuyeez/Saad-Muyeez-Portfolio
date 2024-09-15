d78cb67e32f91440b1c4fe7521afe48811025917
SaadMuyeez <saadmuyeez@gmail.com>
2024-09-15 15:59:25 +0530

    Add Initail Files

---
+++ script.js
@@ -0,0 +1,9 @@
+function openaction() {
+    let links = document.getElementById("Links")
+    links.style.display="block";
+}
+
+function closeaction() {
+    let links = document.getElementById("Links")
+    links.style.display="none";
+}
